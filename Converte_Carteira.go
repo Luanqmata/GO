882620c97e80bf0f4248c9451a48509d2e28dbc0
*Luan Calazans* <138558778+Luanqmata@users.noreply.github.com>
2024-06-23 18:56:38 -0300

    Create Converte_Carteira.go

---
+++ Converte_Carteira.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"meugo/crypto/base58"
+
+	"github.com/btcsuite/btcd/btcec"
+)
+
+func main() {
+	privateKeyHex := "000000000000000000000000000000000000000000000000000000000001764f"
+
+	privateKeyBytes, err := hex.DecodeString(privateKeyHex)
+	if err != nil {
+		fmt.Println("Erro ao decodificar chave privada:", err)
+		return
+	}
+
+	privateKey, _ := btcec.PrivKeyFromBytes(btcec.S256(), privateKeyBytes)
+
+	publicKey := privateKey.PubKey()
+
+	publicKeyBytes := publicKey.SerializeCompressed()
+	
+	address := base58.Encode(publicKeyBytes)
+
+	fmt.Println("Endereço Bitcoin correspondente à chave privada:", address)
+}
